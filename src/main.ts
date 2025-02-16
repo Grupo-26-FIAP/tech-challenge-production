933ea222fb393ccd476fd157548910427bbc806a
Jhoni Faria <jhonifarias16@gmail.com>
2025-02-16 20:18:46 -0300

    adjust health prefix

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('/production/api/v1/');
+  app.setGlobalPrefix('/production/api/v1/', { exclude: ['health'] });
 
   initializeCors(app);
   initializeSwagger(app);
